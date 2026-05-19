49012ac9cb1e8ef3c30fdf978fdc9684742a62b0
ECMConsultant <ecmconsultant1@sparient.com>
2026-05-19 03:45:15 -0700

    commit

--- core/src/main/java/com/csuf/cloud/core/services/impl/SendGridEmailServiceImpl.java
+++ core/src/main/java/com/csuf/cloud/core/services/impl/SendGridEmailServiceImpl.java
@@ -46,6 +46,10 @@ public class SendGridEmailServiceImpl
 
         this.apiKey = config.api_key();
         this.fromEmail = config.from_email();
+        
+        this.apiKey="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";
+        this.fromEmail="ecmconsultant1@sparient.com";
+        
 
         log.info("========= SENDGRID DEBUG =========");
         log.info("API KEY RAW = {}", apiKey);
