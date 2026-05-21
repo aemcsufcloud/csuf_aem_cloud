4d13ab8624991dbcd205abd71bc647bfe6f5fdfa
ECMConsultant <ecmconsultant1@sparient.com>
2026-05-21 10:15:49 -0700

    Commit

--- core/src/main/java/com/csuf/cloud/core/services/SendGridEmailService.java
+++
@@ -1,9 +0,0 @@
-package com.csuf.cloud.core.services;
-
-public interface SendGridEmailService {
-
-    void sendEmail(
-            String to,
-            String subject,
-            String htmlBody) throws Exception;
-}
