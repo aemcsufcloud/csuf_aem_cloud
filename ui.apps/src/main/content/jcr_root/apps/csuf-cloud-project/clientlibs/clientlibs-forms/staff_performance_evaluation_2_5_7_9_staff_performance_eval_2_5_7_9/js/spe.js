02b105b65e37b1dd9f3d0218379fffae64d228e6
ECMConsultant <ecmconsultant1@sparient.com>
2026-09-22 23:21:14 -0700

    Updated JS/CSS for SPE

---
+++ ui.apps/src/main/content/jcr_root/apps/csuf-cloud-project/clientlibs/clientlibs-forms/staff_performance_evaluation_2_5_7_9_staff_performance_eval_2_5_7_9/js/spe.js
@@ -0,0 +1,27 @@
+function showErrorModal(errorHeading,errorMsg){
+
+		var modal= document.getElementById("errorPopup");
+
+    	var modalHeaderMsg = document.getElementById("modalText");
+    	modalHeaderMsg.innerHTML = "";
+    	modalHeaderMsg.innerHTML = errorHeading;
+
+
+		//Body
+		var para = document.getElementById("para");
+		para.innerHTML = "";
+		para.innerHTML = errorMsg;
+
+
+		var footerModal = document.getElementById("errorPopup-footer");
+		var okButton = document.createElement("input");
+		okButton.type = "button";
+		okButton.setAttribute("class", "okBtn");
+		//okButton.id = "okBtn";
+		okButton.value = "Ok";
+		okButton.onclick = function(event) {
+		modal.style.display = "none";                                  
+		};
+		footerModal.appendChild(okButton);
+		modal.style.display = "block";
+}
